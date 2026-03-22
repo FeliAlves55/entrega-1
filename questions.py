89e3d74f62ef1542254de1764e091371512ca7df
feli <felipealves259259@gmail.com>
2026-03-22 12:56:19 -0300

    Trabajo Completo

--- questions.py
+++ questions.py
@@ -1,11 +1,16 @@
 import random
 import string 
+
 rango = string.ascii_letters
 categories = {
 "Programacion": ["python","programa","variable","funcion","bucle"],
 "Valores": ["cadena","entero","lista",]
 }
-#Mostrar Caegorias
+
+print("¡Bienvenido al Ahorcado!")
+print()
+print("Categorias:" )
+#Mostrar Categorias
 for elem in categories:
     print("|",elem)
 category_ch = input("Escribe el nombre de la categoria a jugar: ")
@@ -16,47 +21,72 @@ while category_ch not in categories:
 
 #Seleccion de palabra al azar
 category_play = categories[category_ch]
-word = random.choice(category_play)
+words_mix= random.sample(category_play, len(category_play))
 
-guessed = []
-attempts = 6
+index= 0
 puntaje = 0
-print("¡Bienvenido al Ahorcado!")
-print()
-#Mostrar categorias
 
-while attempts > 0:
-    # Mostrar progreso: letras adivinadas y guiones para las que faltan
-    progress = ""
-    for letter in word:
-        if letter in guessed:
-            progress += letter + " "
+while index < len(words_mix):
+    guessed = []
+    attempts = 6
+    word = words_mix[index]
+    
+    while attempts > 0 :
+
+        # Mostrar progreso: letras adivinadas y guiones para las que faltan
+        progress = ""
+        for letter in word:
+            if letter in guessed:
+                progress += letter + " "
+            else:
+                progress += "_ "
+        print(progress)
+
+        # Verificar si el jugador ya adivinó la palabra completa
+        if "_" not in progress:
+            print("¡Ganaste!")
+            puntaje = puntaje + 6
+            print(f"Tu puntaje es de: {puntaje}")
+            print()
+            ask = input("Quieres volver a jugar?| si/no: ")
+            if ask == "si":
+              index +=1
+              guessed = []
+              attempts = 6
+              word = words_mix[index]
+            elif ask == "no":
+              print()
+              print("Buen juego hasta pronto!") 
+              break
+
         else:
-            progress += "_ "
-    print(progress)
-    # Verificar si el jugador ya adivinó la palabra completa
-    if "_" not in progress:
-        print("¡Ganaste!")
-        puntaje = puntaje + 6
-        break
-    print(f"Intentos restantes: {attempts}")
-    print(f"Letras usadas: {', '.join(guessed)}")
-    letter = input("Ingresá una letra: ")
-    if letter in guessed:
-        print("Ya usaste esa letra.")
-    if len(letter) == 1 and letter in rango:
-        if letter in word:
-         guessed.append(letter)
-         print("¡Bien! Esa letra está en la palabra.")
-        else:
-         guessed.append(letter)
-         attempts -= 1
-         print("Esa letra no está en la palabra.")
-         puntaje = puntaje - 1
-    else:
-       print("Entrada no valida.")
+         print(f"Intentos restantes: {attempts}")
+         print(f"Letras usadas: {', '.join(guessed)}")
+         letter = input("Ingresá una letra: ")
+         if letter in guessed:
+            print("Ya usaste esa letra.")
+         if len(letter) == 1 and letter in rango:
+            if letter in word:
+                guessed.append(letter)
+                print("¡Bien! Esa letra está en la palabra.")
+            else:
+                guessed.append(letter)
+                attempts -= 1
+                print("Esa letra no está en la palabra.")
+                puntaje = puntaje - 1
+         else:
+            print()
+            print("¡¡Entrada no valida!!.")
+        print()
+
+    print(f"¡Perdiste! La palabra era: {word}")
+    puntaje = 0
+    print(f"Tu puntaje es de: {puntaje}")
     print()
-else:
- print(f"¡Perdiste! La palabra era: {word}")
- puntaje = 0
-print(f"Tu puntaje es de: {puntaje}")
+    ask = input("Quieres volver a jugar?| si/no: ")           
+    if ask == "si":
+        index +=1
+    elif ask == "no":  
+        print()      
+        print("Buen juego hasta pronto!") 
+        break
