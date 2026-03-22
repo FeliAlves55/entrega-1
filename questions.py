f9522951933b10f6cfe2aa98846ed65850ce9ea2
feli <felipealves259259@gmail.com>
2026-03-22 11:10:00 -0300

    inciso 2

--- questions.py
+++ questions.py
@@ -14,6 +14,7 @@ words = [
 word = random.choice(words)
 guessed = []
 attempts = 6
+puntaje = 0
 print("¡Bienvenido al Ahorcado!")
 print()
 while attempts > 0:
@@ -28,6 +29,7 @@ while attempts > 0:
     # Verificar si el jugador ya adivinó la palabra completa
     if "_" not in progress:
         print("¡Ganaste!")
+        puntaje = puntaje + 6
         break
     print(f"Intentos restantes: {attempts}")
     print(f"Letras usadas: {', '.join(guessed)}")
@@ -42,12 +44,11 @@ while attempts > 0:
          guessed.append(letter)
          attempts -= 1
          print("Esa letra no está en la palabra.")
+         puntaje = puntaje - 1
     else:
        print("Entrada no valida.")
     print()
 else:
  print(f"¡Perdiste! La palabra era: {word}")
-
-
- #sdbasgiasbibsvhsbvkbv
- #asvasnvsv
+ puntaje = 0
+print(puntaje)
